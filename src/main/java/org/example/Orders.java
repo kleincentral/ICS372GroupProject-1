9aff47c4123f60930680e82a12fa3e1507fb1402
MarkKlein <kleincentral@gmail.com>
2026-02-11 00:56:56 -0600

    made orders fit with the current Order class provided. Fixed ITEM for usage in Orders' Code

---
+++ src/main/java/org/example/Orders.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import org.example.model.Order;
+import org.example.model.OrderStatus;
+import org.example.model.OrderType;
+import java.util.List;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Orders {
+    private Order[] Orders;
+    private int index = 0;
+
+    // constructor
+    public Orders() {
+        this.Orders = new Order[1000];
+    }
+
+    public void addOrder(OrderType type, long time, List<Item> items, String orderID) {
+        Orders[index] = new Order(orderID, type, time, items);
+        index++;
+    }
+
+    public Order[] getOrders() {
+        return Orders;
+    }
+
+    private Order findByID(String orderID) {
+        try {
+            for (int i = 0; i < Orders.length; i++) {
+                if (Orders[i] == null) {
+                    return null;
+                }
+                else if (Orders[i].getOrderId().equals(orderID)) {
+                    return Orders[i];
+                }
+            }
+        } catch (Exception E) {
+            throw new RuntimeException("An error occurred: " + E);
+        }
+        return null;
+    }
+
+    public OrderStatus getCompletionStatus(String orderID) {
+        return findByID(orderID).getStatus();
+    }
+
+    // Returns true once the completion status has been updated.
+    // You must pass an INT for the orderID, however if you want to just use
+    // the last order you looked up in completion status you can enter -1
+    public boolean updateCompletion(OrderStatus completionStatus, String orderID) {
+        try {
+            findByID(orderID).setStatus(completionStatus);
+            return true;
+        } catch (Exception E) {
+            return false;
+        }
+    }
+
+
+}  //item price
+
